25e0ff44b6bb19f8199f87e7e23aff5b2e0d63e0
Davinia pd <delgadavi@gmail.com>
2024-09-10 10:23:28 +0000

    creada column favorita y eliminadas las otras pivotes

--- src/models.py
+++ src/models.py
@@ -16,24 +16,9 @@ class User(Base):
     email = Column(String(250), nullable=False, unique=True)
     password = Column(String(250), nullable=False)
 
-class Character(Base):
-    __tablename__ = 'Characters'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-    specie = Column(String(250), nullable=False)
-    role = Column(String(250), nullable=False)
-    lifeStatus = Column(String(250), nullable=False)
-    gender = Column(String(250), nullable=False)
-    homeworld_id = Column(Integer, ForeignKey('planet.id'))
-
-class User_Character(Base):
-    __tablename__ = 'User_Character'
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    character_id = Column(Integer, ForeignKey('character.id'))
 
 class Planet(Base):
-    __tablename__ = 'Planets'
+    __tablename__ = 'planets'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False) 
     galactic_location = Column(String(250), nullable=False)
@@ -42,14 +27,19 @@ class Planet(Base):
     native_species = Column(String(250), nullable=False)
     government  = Column(String(250), nullable=False)
 
-class User_Planet(Base):
-    __tablename__ = 'User_Planet'
+class Character(Base):
+    __tablename__ = 'characters'
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    planet_id = Column(Integer, ForeignKey('planet.id'))
+    name = Column(String(250), nullable=False)
+    specie = Column(String(250), nullable=False)
+    role = Column(String(250), nullable=False)
+    lifeStatus = Column(String(250), nullable=False)
+    gender = Column(String(250), nullable=False)
+    homeworld_id = Column(Integer, ForeignKey('planet.id'))
+    planet = relationship(Planet)
 
 class Vehicle(Base):
-    __tablename__ = 'Vehicles'
+    __tablename__ = 'vehicles'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     vehicles_class = Column(String(250), nullable=False)
@@ -58,11 +48,17 @@ class Vehicle(Base):
     weapons = Column(String(250), nullable=False)
     passengers  = Column(String(250), nullable=False)
 
-class User_Vehicle(Base):
-    __tablename__ = 'User_Vehicle'
+class Favorite(Base):
+    __tablename__ = 'favorite'
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    character_id = Column(Integer, ForeignKey('character.id'))
+    character = relationship(Character)
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    planet = relationship(Planet)
     vehicle_id = Column(Integer, ForeignKey('vehicle.id'))
+    vehicle = relationship(Vehicle)
 
 
 ## Draw from SQLAlchemy base
